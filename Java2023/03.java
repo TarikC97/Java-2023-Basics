f80878c54cbc9272b3da21b24615529f104dcb17
unknown <tarikcuric97@gmail.com>
2023-10-15 15:30:42 +0200

    Java2023 TeluskoCourse 15-10-2023

--- Java2023/03.java
+++ Java2023/03.java
@@ -1,8 +1,18 @@
 class Proba {
-    public static void main(String a[]) {
-        int broj1 = 5;
-        int broj2 = 3;
-        System.out.println("Zbir je:"+ (broj1+broj2));
+    public static void main(String args[]) {
+
+        //literals
+        // int a = 258;
+        // byte k = (byte)a;//Casting
+        // // int d = k//Conversion;
+
+        
+        // float f =5.6f;
+        // int t =(int)f;//Casting
+        byte a=10;
+        byte b=30;
+        int rez = a*b;//Type Conversion:(a * b goes out of range, java converts it to int).
+        System.out.println(rez);
     }
 
 }
