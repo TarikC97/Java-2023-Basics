6732ba774da5ad8986ac1d273160996c2832aa62
unknown <tarikcuric97@gmail.com>
2023-10-16 00:33:46 +0200

    Java2023 TeluskoCourse 16-10-2023

--- Java2023/TypeConversion.java
+++ Java2023/TypeConversion.java
@@ -6,13 +6,15 @@ class Proba {
         // byte k = (byte)a;//Casting
         // // int d = k//Conversion;
 
-        
         // float f =5.6f;
         // int t =(int)f;//Casting
         byte a=10;
         byte b=30;
         int rez = a*b;//Type Conversion:(a * b goes out of range, java converts it to int).
         System.out.println(rez);
+
+        //++num; - PreIncrement, num++; - PostIncrement;
+        //int x=5;int y=6; boolean rez = x>5 => println(rez)
     }
 
 }
