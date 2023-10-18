43a7c9adecefe9a5af95bda8764873e6c4fd5d75
unknown <tarikcuric97@gmail.com>
2023-10-18 20:12:46 +0200

    Java2023 TeluskoCourse 18-10-2023

---
+++ Java2023/Conditionals.java
@@ -0,0 +1,33 @@
+  class Conditional{      
+    
+        public static void main(String args[]){   
+        //++num; - PreIncrement, num++; - PostIncrement;
+        //Relational Operators(>,==,<)
+        //int x=5;int y=6; boolean rez = x>5 => println(rez)
+        //Logical Operators(&&,||,!)
+        //Conditional Statements (If,Else,IfElse)
+        //Ternary Operators ?,:
+        //Switch
+            int n =2;
+                switch(n){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;//So it stops executing rest cases
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            default:
+                System.out.println("Rest");
+            }   
+        }
+        
+  }
--- Java2023/TypeConversion.java
+++ Java2023/TypeConversion.java
@@ -1,6 +1,5 @@
 class Proba {
     public static void main(String args[]) {
-
         //literals
         // int a = 258;
         // byte k = (byte)a;//Casting
@@ -12,11 +11,6 @@ class Proba {
         byte b=30;
         int rez = a*b;//Type Conversion:(a * b goes out of range, java converts it to int).
         System.out.println(rez);
-
-        //++num; - PreIncrement, num++; - PostIncrement;
-        //int x=5;int y=6; boolean rez = x>5 => println(rez)//Relational Operators(>,==,<)
-        //Logical Operators(&&,||,!)
-        //Conditional Statements (If,Else)
     }
 
 }
