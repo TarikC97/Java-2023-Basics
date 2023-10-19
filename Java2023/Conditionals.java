cf400483fd7dbd90150ce3b365d61584410678d5
unknown <tarikcuric97@gmail.com>
2023-10-19 23:57:54 +0200

    Java2023 TeluskoCourse 19-10-2023

--- Java2023/Conditionals.java
+++ Java2023/Conditionals.java
@@ -7,27 +7,36 @@
         //Logical Operators(&&,||,!)
         //Conditional Statements (If,Else,IfElse)
         //Ternary Operators ?,:
-        //Switch
-            int n =2;
-                switch(n){
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;//So it stops executing rest cases
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            default:
-                System.out.println("Rest");
-            }   
-        }
-        
+        //Switch old
+        //     int n =2;
+        //         switch(n){
+        //     case 1:
+        //         System.out.println("Monday");
+        //         break;
+        //     case 2:
+        //         System.out.println("Tuesday");
+        //         break;//So it stops executing rest cases
+        //     case 3:
+        //         System.out.println("Wednesday");
+        //         break;
+        //     case 4:
+        //         System.out.println("Thursday");
+        //         break;
+        //     case 5:
+        //         System.out.println("Friday");
+        //         break;
+        //     default:
+        //         System.out.println("Rest");
+        //     }   
+        // }
+        //Switch new 
+        String day = "Monday";
+        String result ="";
+        result = switch(day){
+            case "Saturday","Sunday" -> "6am";
+            case "Monday" -> "7am";        
+            default -> "8am";
+        };
+        System.out.println(result);
+    } 
   }
