1e34d67e8632ff13167adf7901104e0c8177b761
unknown <tarikcuric97@gmail.com>
2023-10-29 20:28:36 +0100

    Java2023 29-10-2023

---
+++ Java2023/ClassAndObject.java
@@ -0,0 +1,5 @@
+public class ClassAndObject {
+    public static void main(String args[]) {
+        // Objects - Have Properties and Behaviour.
+    }
+}
--- Java2023/Loops.java
+++ Java2023/Loops.java
@@ -2,27 +2,40 @@ class Loop {
     public static void main(String args[]) {
         // loops:
         // While Loop
-        int i = 1;
-        while (i < 5) {
-            System.out.println("Test:" + i);
-            int j = 1;
-            while (j < 4) {
-                System.out.println("Hello" + j);
-                j++;
-            }
-            // When Second condition is false, i++ go to first condition.
-            i++;
-        }
+        // int i = 1;
+        // while (i < 5) {
+        // System.out.println("Test:" + i);
+        // int j = 1;
+        // while (j < 4) {
+        // System.out.println("Hello" + j);
+        // j++;
+        // }
+        // // When Second condition is false, i++ go to first condition.
+        // i++;
+        // }
         // Because i<5 is false, when i is 5,prints 5 after while
         // Since i is declared outside of the loop, value will be maintained
-        System.out.println("After" + i);
+        // System.out.println("After" + i);
 
-        //Do While Loop
-        int p = 5;
-        do {
-            System.out.println("Do While" + p);
-            p++;
-        } while (p < 5);
+        // //Do While Loop
+        // int p = 5;
+        // do {
+        // System.out.println("Do While" + p);
+        // p++;
+        // } while (p < 5);
+
+        // For Loop:
+        // (Starting Value;Limit; Counter)
+        int i = 1;
+        for (; i <= 5;) {
+            // It executes startValue, limit, then block
+            // At end it executes counter.
+            System.out.println("Day:" + i);
+            for (int j = 1; j <= 9; j++) {
+                System.out.println((j + 8) + "-" + (j + 9));
+            }
+            i++;
+        }
 
     }
 }
