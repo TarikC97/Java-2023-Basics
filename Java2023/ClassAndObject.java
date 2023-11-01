83fd5a0a0093330a706386994d05217b2db83ec0
unknown <tarikcuric97@gmail.com>
2023-11-01 21:22:22 +0100

    Java2023 01-11-2023

--- Java2023/ClassAndObject.java
+++ Java2023/ClassAndObject.java
@@ -1,6 +1,25 @@
+class Calculator {
+    // Behaviour - Adding two numbers
+    // You Design your objects in class
+    int a;
+    int b;
+
+    // Methods Overloading
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
 public class ClassAndObject {
     public static void main(String args[]) {
         // Objects - Have Properties and Behaviour.
-        //
+        // Creating new Object of type Calculator
+        Calculator obj = new Calculator();
+        int rez = obj.add(5, 5);
+        System.out.println("Rezult:" + rez);
     }
 }
