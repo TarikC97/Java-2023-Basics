f724c3b6b088cd92843e5421797de33fbfae0aef
unknown <tarikcuric97@gmail.com>
2023-11-03 16:53:35 +0100

    Java2023 03-11-2023

---
+++ Java2023/Arrays.java
@@ -0,0 +1,10 @@
+class Arrays {
+    public static void main(String args[]) {
+        int nums[] = { 5, 6, 7 };
+        int nums2[] = new int[5];
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+
+    }
+}
