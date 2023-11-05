84af06e7b58817ec73bc3cdd23d19d00241245d8
unknown <tarikcuric97@gmail.com>
2023-11-05 02:08:12 +0100

    Java2023 05-11-2023

--- Java2023/Arrays.java
+++ Java2023/Arrays.java
@@ -1,10 +1,42 @@
 class Arrays {
     public static void main(String args[]) {
-        int nums[] = { 5, 6, 7 };
-        int nums2[] = new int[5];
-        for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i]);
-        }
+        // int nums[] = { 5, 6, 7 };
+        // // int nums2[] = new int[5];
+        // for (int i = 0; i < nums.length; i++) {
+        // System.out.println(nums[i]);
+        // }
+        // 3. 3D Array
+        // int arr[][][] = new int[3][4][5];
+        // for (int j = 0; j < arr.length; j++) {
+        // for (int k = 0; k < arr[j].length; k++) {
+        // for (int i = 0; i < arr[j][k].length; i++) {
+        // arr[j][k][i] = (int) (Math.random() * 10);
+        // System.out.print(arr[j][k][i] + " ");
+        // }
 
+        // }
+        // System.out.println();
+        // }
+        // 1. Jagged Array
+        // int multi[][] = new int[3][];
+        // multi[0] = new int[3];
+        // multi[1] = new int[4];
+        // multi[2] = new int[2];
+        // for (int j = 0; j < multi.length; j++) {
+        // for (int k = 0; k < multi[j].length; k++) {
+        // multi[j][k] = (int) (Math.random() * 10);
+        // System.out.print(multi[j][k] + " ");
+        // }
+        // System.out.println();
+        // }
+        // 2. Enhanced loop - Multi Dimensional Arrays
+        // for (int n[] : multi) {
+        // for (int m : n) {
+        // m = (int) (Math.random() * 10);
+        // System.out.print(m + " ");
+        // }
+        // System.out.println();
+        // }
+        // Array Setbacks
     }
 }
