06f81e1118d0f3822e396b60a252c29fb277d7ee
unknown <tarikcuric97@gmail.com>
2023-11-08 14:33:26 +0100

    Java2023 08-11-2023

--- Java2023/Strings.java
+++ Java2023/Strings.java
@@ -6,9 +6,16 @@ public class Strings {
         // System.out.println(name.charAt(0));
         // System.out.println(name.concat("Muric"));
         // Object is created for us behind the scenes
+        // Creates new object with new Adress.
+        // Immutable String
+        // Once object created it cannot be changed.
         String surname = "Curic";
         surname = surname + "Tarik";
         System.out.println(surname);
-
+        // String Constant Pool in Heap
+        // One object for same values strings.
+        String s1 = "Ferid";
+        String s2 = "Ferid";
+        System.out.println(s1 == s2);
     }
 }
