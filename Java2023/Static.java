e74ede6be7007cd987c64d1cdbc57dc1cdf21f9b
unknown <tarikcuric97@gmail.com>
2023-11-11 23:08:15 +0100

    Java2023 11-11-2023

--- Java2023/Static.java
+++ Java2023/Static.java
@@ -28,13 +28,14 @@ class Mobile {
 }
 
 public class Static {
-    public static void main(String args[]) {
-        Mobile obj1 = new Mobile();
-        obj1.brand = "Apple";
-        obj1.price = 1000;
-        Mobile.name = "SmartPhone";
-        // Two calls to constructor, one to static block
-        Mobile obj2 = new Mobile();
+    public static void main(String args[]) throws ClassNotFoundException {
+        // Used to load class
+        Class.forName("Mobile");
+        // Mobile obj1 = new Mobile();
+        // obj1.brand = "Apple";
+        // obj1.price = 1000;
+        // Mobile.name = "SmartPhone";
+        // // Two calls to constructor, one to static block
         // Mobile obj2 = new Mobile();
         // obj2.brand = "Samsung";
         // obj2.price = 800;
