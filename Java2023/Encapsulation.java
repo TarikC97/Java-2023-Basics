d0f1909db7d1b7d71fa1f999d50ae332b907a21b
unknown <tarikcuric97@gmail.com>
2023-11-16 14:43:22 +0100

    Java2023 16-11-2023

--- Java2023/Encapsulation.java
+++ Java2023/Encapsulation.java
@@ -1,29 +1,40 @@
 class Human {
+    // Instance Variables
     private int age;
     private String name;
+
+    public Human() {
+        System.out.println("Constructor");
+        age = 15;
+        name = "Proba";
+    }
 
     public int getAge() {
         return age;
     }
 
-    public void setAge(int a) {
-        age = a;
+    public void setAge(int age) {
+        // Local variable
+        // In case of confusion it is always local variable
+        this.age = age;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String n) {
-        name = n;
+    public void setName(String name) {
+        this.name = name;
     }
 }
 
 public class Encapsulation {
     public static void main(String[] args) {
         Human obj = new Human();
+        // By default null values for unsigned values for variables.
+        System.out.println(obj.getName() + ":" + obj.getAge());
         obj.setAge(25);
         obj.setName("Tarik");
-        System.out.println(obj.getName() + ":" + obj.getAge());
+        // System.out.println(obj.getName() + ":" + obj.getAge());
     }
 }
