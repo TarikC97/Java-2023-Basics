6b6fca044163c3073e799ac958d70282c82e5bea
unknown <tarikcuric97@gmail.com>
2023-11-23 14:30:01 +0100

    Java2023 23-11-2023

--- Java2023/SuperMethod.java
+++ Java2023/SuperMethod.java
@@ -1,3 +1,33 @@
+class A extends Object {
+    public A() {
+        super();
+        System.out.println("Class A");
+    }
+
+    public A(int n) {
+        super();
+        System.out.println("in A int");
+    }
+}
+
+class B extends A {
+    public B() {
+        super();
+        System.out.println("Main B Class");
+    }
+
+    public B(int n) {
+        this();// Execute constructor of same Class.
+        System.out.println("in B int");
+    }
+}
+
 public class SuperMethod {
-    
+    public static void main(String args[]) {
+        // Creating obj of Super class, It calls
+        // super and subclass constructor
+        // B obj = new B(5);
+        B obj = new B(5);
+    }
+
 }
