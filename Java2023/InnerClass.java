b0f3fe53ab4cfe75a657e35ae0817f47ffb47f89
unknown <tarikcuric97@gmail.com>
2023-11-30 14:37:33 +0100

    Java2023 30-11-2023

--- Java2023/InnerClass.java
+++ Java2023/InnerClass.java
@@ -1,3 +1,23 @@
+class N {
+    int age;
+
+    public void show() {
+        System.out.println("Show");
+    }
+
+    static class M {
+        public void config() {
+            System.out.println("Config");
+        }
+    }
+}
+
 public class InnerClass {
-    
+    public static void main(String[] args) {
+        N obj = new N();
+        // N.M obj1 = obj.new M();
+        N.M obj1 = new N.M();
+        obj1.config();
+
+    }
 }
