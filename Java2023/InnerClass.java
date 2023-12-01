21d2c468a101f1c6241c28cb80e8edcca796e229
unknown <tarikcuric97@gmail.com>
2023-12-01 01:01:41 +0100

    Java2023 01-12-2023

--- Java2023/InnerClass.java
+++ Java2023/InnerClass.java
@@ -1,23 +1,41 @@
-class N {
-    int age;
+abstract class N {
+    // int age;
 
-    public void show() {
-        System.out.println("Show");
-    }
+    public abstract void show();
 
-    static class M {
-        public void config() {
-            System.out.println("Config");
-        }
-    }
+    public abstract void show2();
+
+    // static class M {
+    // public void config() {
+    // System.out.println("Config");
+    // }
+    // }
 }
 
 public class InnerClass {
     public static void main(String[] args) {
-        N obj = new N();
-        // N.M obj1 = obj.new M();
-        N.M obj1 = new N.M();
-        obj1.config();
+        // N obj = new N();
+        // // N.M obj1 = obj.new M();
+        // N.M obj1 = new N.M();
+        // obj1.config();
+        // AnonymusInnerClass
+        // N obj = new N() {
+        // public void show() {
+        // System.out.println("New Show");
+        // }
+        // };
+        // obj.show();
+        // Abstract and AnonymusInnerClass
+        N obj = new N() {
+            public void show() {
+                System.out.println("New Show");
+            }
 
+            public void show2() {
+                System.out.println("New Show2");
+            }
+        };
+        obj.show();
+        obj.show2();
     }
 }
