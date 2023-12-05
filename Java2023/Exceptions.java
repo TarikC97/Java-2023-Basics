dd11cdd4c0fd1f67805b763b5ee7b37d3ad4444c
unknown <tarikcuric97@gmail.com>
2023-12-05 13:32:40 +0100

    Java2023 05-12-2023

--- Java2023/Exceptions.java
+++ Java2023/Exceptions.java
@@ -7,12 +7,22 @@ public class Exceptions {
         // Runtime errors
         // Your app opens file , and that file is no longer there
         // That's runtime error, execution will stop in between
-        int i = 0;
+        int i = 2;
         int j = 0;
+        int arr[] = new int[5];
+        String str = null;
         try {
             j = 18 / i;
-        } catch (Exception obj) {
+            System.out.println(str.length());
+            System.out.println(arr[1]);
+            System.out.println(arr[5]);
+        } catch (ArithmeticException obj) {
+
             System.out.println("Something went wrong!" + obj);
+        } catch (ArrayIndexOutOfBoundsException obj) {
+            System.out.println("Stay in your limit");
+        } catch (Exception e) {
+            System.out.println("Something Went wrong" + e);
         }
         System.out.println(j);
         System.out.println("Test");
