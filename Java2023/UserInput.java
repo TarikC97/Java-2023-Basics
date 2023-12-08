ac5b8c713a153952e46a2a314a0898511b877399
unknown <tarikcuric97@gmail.com>
2023-12-08 23:56:16 +0100

    Java2023 08-12-2023

--- Java2023/UserInput.java
+++ Java2023/UserInput.java
@@ -1,10 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class UserInput {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        // throws IO Exception
         // int num = System.in.read();
         // We are passing Input Stream object in
         // InputStreamReader in = new InputStreamReader(System.in);
@@ -12,9 +13,25 @@ public class UserInput {
         // int num = Integer.parseInt(bf.readLine());
         // System.out.println(num);
         // bf.close();
-        Scanner obj = new Scanner(System.in);
-        int num = obj.nextInt();
-        System.out.println(num);
+        // Scanner obj = new Scanner(System.in);
+        // int num = obj.nextInt();
+        // System.out.println(num);
+        int num = 0;
+        // BufferedReader br = null;
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));) {
+            // m = 18 / n;
+            // System.out.println("Try Block");
+            // br = new BufferedReader(new InputStreamReader(System.in));
+            num = Integer.parseInt(br.readLine());
+            System.out.println(num);
+        }
+        // } catch (Exception e) {
+        // System.out.println("Something went wrong.");}
+        // finally {
+        // System.out.println("Finally Block");
+        // br.close();
+
+        // }
     }
 
 }
