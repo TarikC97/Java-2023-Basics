ef60d02042f7eed2685033078ea8fdc1ac1be373
unknown <tarikcuric97@gmail.com>
2023-12-09 23:44:33 +0100

    Java2023 09-12-2023

--- Java2023/Threads.java
+++ Java2023/Threads.java
@@ -1,6 +1,28 @@
+class T extends Thread {
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Threads");
+        }
+
+    }
+}
+
+class Q extends Thread {
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Multi Threads ");
+        }
+
+    }
+}
+
 public class Threads {
 
     public static void main(String[] args) {
+        T obj = new T();
+        Q obj2 = new Q();
+        obj.start();
+        obj2.start();
 
     }
 }
