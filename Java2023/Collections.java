23aa14bffb2b7a8f567bc4c70dd9d3c830e8c084
unknown <tarikcuric97@gmail.com>
2023-12-12 23:52:55 +0100

    Java2023 12-12-2023

--- Java2023/Collections.java
+++ Java2023/Collections.java
@@ -3,9 +3,12 @@
 // import java.util.HashSet;
 // import java.util.Collection;
 // import java.util.List;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.TreeSet;
+// import java.util.Iterator;
+// import java.util.Set;
+// import java.util.TreeSet;
+import java.util.HashMap;
+// import java.util.Hashtable;
+import java.util.Map;
 
 public class Collections {
 
@@ -15,10 +18,10 @@ public class Collections {
         // HashSet - Gives unique values
         // TreeSet - Gives sorted values
         // Collection can be used instead of Set
-        Set<Integer> nums = new TreeSet<Integer>();
-        nums.add(6);
-        nums.add(5);
-        nums.add(5);
+        // Set<Integer> nums = new TreeSet<Integer>();
+        // nums.add(6);
+        // nums.add(5);
+        // nums.add(5);
         // Iterator<Integer> values = nums.iterator();
         // while (values.hasNext()) {
         // System.out.println(values.next());
@@ -30,6 +33,24 @@ public class Collections {
         // System.out.println(num);
         // }
         // System.out.println(nums);
+
+        // Map-Is a collection of a key value pair.
+        // Keys are unique
+        Map<String, Integer> students = new HashMap<>();
+        // Map<String, Integer> students2 = new Hashtable<>();
+        students.put("Tc", 25);
+        students.put("Ac", 24);
+        students.put("Zh", 26);
+        students.put("Eh", 27);
+        students.put("Eh", 25);
+        // System.out.println(students.get("Tc"));
+        // Method to get all the keys
+        // System.out.println(students.keySet());
+        // All values
+        // System.out.println(students.values());
+        for (String key : students.keySet()) {
+            System.out.println(key + ":" + students.get(key));
+        }
 
     }
 }
