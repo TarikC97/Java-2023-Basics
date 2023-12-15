318e26a8cf8c619b026105fcafed45dbe9bb3a45
unknown <tarikcuric97@gmail.com>
2023-12-15 11:46:04 +0100

    Stream Api 15-12

---
+++ Java2023/Var.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Var {
+    // var num =10;
+    public static void main(String[] args) {
+        // var obj = new ArrayList<>();
+        int a = 5;
+        var b = 6;
+        int c;
+        // var d;
+        String var = "Tc";
+        // int nums[] = new int[10];
+        var nums = new int[10];
+        // var obj = new A();
+        System.out.println(a + "-" + b + "-" + var);
+    }
+}
