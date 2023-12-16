720189a5efc520f941056e19c57b53f01456b464
unknown <tarikcuric97@gmail.com>
2023-12-16 12:36:41 +0100

    Sealed Classes 16-12

---
+++ Java2023/Sealed.java
@@ -0,0 +1,25 @@
+sealed class Se extends Thread implements Cloneable permits De {
+
+}
+
+non-sealed class De extends Se {
+
+}
+
+class Pe {
+
+}
+
+sealed interface Mup permits Put {
+
+}
+
+non-sealed interface Put extends Mup {
+
+}
+
+public class Sealed {
+    public static void main(String[] args) {
+
+    }
+}
--- Java2023/Var.java
+++ Java2023/Var.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
 public class Var {
     // var num =10;
@@ -6,11 +6,11 @@ public class Var {
         // var obj = new ArrayList<>();
         int a = 5;
         var b = 6;
-        int c;
+        // int c;
         // var d;
         String var = "Tc";
         // int nums[] = new int[10];
-        var nums = new int[10];
+        // var nums = new int[10];
         // var obj = new A();
         System.out.println(a + "-" + b + "-" + var);
     }
